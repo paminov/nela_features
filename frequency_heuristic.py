61c36387b0776047f73b2f4212dd832a0a493631
Parvizsho Aminov <paminov@cisco.com>
2019-12-17 19:34:10 -0800

    fixing import

--- frequency_heuristic.py
+++ frequency_heuristic.py
@@ -7,7 +7,7 @@ from pydrive.drive import GoogleDrive
 from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import Train_test_split
+from sklearn.model_selection import train_test_split
 from .utils import *
 
 
